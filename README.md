1f1a66d2bcfe67f6845d25932be49303c8ab2b0a
Alan Janis <alan.janis@gmail.com>
2019-04-04 16:06:42 -0600

    initial

---
+++ README.md
@@ -0,0 +1,37 @@
+# Git Aware Prompt
+
+Based on https://github.com/jimeh/git-aware-prompt
+With improved statuses from https://github.com/Teagan42/GitAwareBash.git
+
+Adds symbols indicating git status:
+
+- [OK]  No local or remote changes
+- [m] Unstaged changes
+- [u] Untracked files
+- [∆] Uncommitted changes
+- [+N] Local changes ready to be pushed
+- [-N] New remote changes
+
+## Preview
+
+![Preview](./preview.png)
+
+
+## Installation
+```
+    mkdir -p "$HOME/.bash"
+    git clone https://github.com/ajanis/git-aware-prompt "$HOME/.bash/git-aware-prompt"
+```
+
+Then in your `~/.bash_profile` add these lines:
+
+```
+    export GITAWAREPROMPT="${HOME}/.bash/git-aware-prompt"
+    source "${GITAWAREPROMPT}/main.sh"
+```
+
+And override `PS1` to look something like this including the vars `$git_branch` and `$git_dirty`
+
+```
+    export PS1="\[$txtred\]\h\[$txtcyn\] \u:\[$txtrst\]\w\[$txtwht\]\[$txtrst\]:\[$txtpur\]\$git_branch\[$txtred\]\$git_status\[$txtrst\] "
+```
---
+++ prompt.sh
@@ -0,0 +1,80 @@
+LAST_GIT_PATH=""
+
+find_git_branch() {
+  local branch
+  local CURRENT_PATH
+
+  CURRENT_PATH="$(pwd)"
+
+  if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
+
+    if [ "$CURRENT_PATH" != "$LAST_GIT_PATH" ]; then
+      (git remote update >/dev/null 2>&1 &)
+    fi
+    LAST_GIT_PATH="$CURRENT_PATH"
+
+    if [[ "$branch" == "HEAD" ]]; then
+      branch='detached*'
+    fi
+    git_branch=" ($branch):"
+  else
+    git_branch=""
+  fi
+}
+
+find_git_dirty() {
+  local status
+  status="$(git status 2> /dev/null)"
+
+
+  git_dirty=""
+
+  if [[ "$status" == "" ]]; then
+      git_status=""
+      return
+
+  else
+  if [[ "$status" =~ "Untracked files" ]]; then
+    git_dirty+="${bldred}u${txtrst}"
+  elif [[ "$status" =~ "Changes not staged for commit" ]]; then
+    git_dirty+="${bldred}m${txtrst}"
+  fi
+
+  if [[ "$status" =~ "Your branch is ahead of" ]]; then
+    local num_a="$(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
+    git_dirty+="${bldcyn}+${num_a}${txtrst}"
+  fi
+
+  if [[ "$status" =~ "Your branch is behind" ]]; then
+    local num_b="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
+    git_dirty+="${bldylw}-${num_b}${txtrst}"
+  fi
+
+  if [[ $"$status" =~ "Changes to be committed" ]]; then
+    git_dirty+="${bldgrn}∆${txtrst}"
+  fi
+
+  if [[ $"$status" =~ "have diverged" ]]; then
+    git_dirty+="${bldred}!=${txtrst}"
+  fi
+
+  local GIT_DIR="$(git rev-parse --git-dir 2> /dev/null)"
+  if [ -n $GIT_DIR ] && test -r $GIT_DIR/MERGE_HEAD; then
+    git_dirty+="${txtpur}⚡︎${txtrst}"
+  fi
+
+  if [[ "$status" =~ "Your branch is up to date with" ]] || [[ "$status" =~ "Your branch is up-to-date with" ]] || [[ "$status" =~ "nothing to commit" ]]; then
+    if [ "$git_dirty" == "" ]; then
+      git_dirty+="${bldgrn}OK${txtrst}"
+    fi
+  fi
+
+  local git_dirty_pfx="${txtwht}[${txtrst}"
+  local git_dirty_sfx="${txtwht}]${txtrst}"
+
+  git_status="${git_dirty_pfx}${git_dirty}${git_dirty_sfx}"
+  fi
+
+}
+
+PROMPT_COMMAND="find_git_branch; find_git_dirty; $PROMPT_COMMAND"
